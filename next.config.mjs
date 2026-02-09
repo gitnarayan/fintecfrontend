0dfa81a152e5e89e4408d418282427ed46db34a8
Narayan Rawat <npsinghrawat007@example.com>
2026-02-09 12:02:45 +0530

    Add Vercel rewrite for API proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://15.207.25.109:5000/api/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
